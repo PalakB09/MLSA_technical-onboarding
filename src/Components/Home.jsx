32eee0943cf4f7eb4840384ebfcd8b3cbe485344
PalakBakshi <palak.bakshi09@gmail.com>
2024-03-19 16:08:27 +0530

    commitchange

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/workspace.jpg";
+import image from "../images/keyboard.jpg";
 
 const imageAltText = "Palak Bakshi : A tech enthusiast and a developer";
 
